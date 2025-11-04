c78f91280d74a9246b477015f1b05cf2398f193c
Davis Kaza <156680027+dkazha@users.noreply.github.com>
2025-11-04 23:11:44 +0100

    Write pseudocode for first iteration

---
+++ substrings.rb
@@ -0,0 +1,7 @@
+def substrings(str, dictionary)
+  # Turn the string into an array using the appropriate method
+  # Iterate over the array and check to see if every word in the dictionary is in every word of the array
+  # Use the reduce method and add a new hash as the initial value for the accumulator
+  # If it is in the dictionary - add it to the hash and add 1 to a counter
+  # Return the hash
+end

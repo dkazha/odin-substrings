82d3541f111eb5d934a5414ae6da28d3e68264a2
Davis Kaza <156680027+dkazha@users.noreply.github.com>
2025-11-05 23:12:46 +0100

    Add functionality to look in several strings

--- substrings.rb
+++ substrings.rb
@@ -2,12 +2,17 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings(str, dictionary)
   found_substrings = Hash.new(0)
-  dictionary.each do |word|
-    if str.include? word
-      found_substrings[word] += 1
+  arr_from_str = str.delete("^a-zA-Z0-9\' ").downcase.split
+  
+  arr_from_str.each do |word|
+    dictionary.each do |dict_word|
+      if word.include? dict_word
+        found_substrings[dict_word] += 1
+      end
     end
   end
   found_substrings
 end
 
 p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
